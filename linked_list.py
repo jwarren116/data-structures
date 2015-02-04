917e9349851fda05feb588c8c2cb234adcee3163
James Warren <jwarren116@gmail.com>
2015-02-04 13:01:09 -0800

    Initial commit with basic singly linked list class outline

---
+++ linked_list.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+
+class SinglyLinked(object):
+    def __init__(self):
+        pass
+
+    def insert(self, val):
+        # insert val at beginning of list
+        pass
+
+    def pop(self):
+        # pops first value from list and returns it
+        pass
+
+    def size(self):
+        # returns length of list
+        pass
+
+    def search(self, val):
+        # return node containing 'val' in list, if present (else None)
+        pass
+
+    def remove(self, node):
+        # remove node from list, wherever it might be
+        pass
+
+    def print_(self):
+        # print list as python tuple literal
+        # (bonus points if you make it appear like "(12, 'sam', 32, 'fred')")
+        pass
