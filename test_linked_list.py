045e0b1f27de074cf5dee36342160ebf47248b3c
James Warren <jwarren116@gmail.com>
2015-02-04 15:59:58 -0800

    Add multi-node tests

--- test_linked_list.py
+++ test_linked_list.py
@@ -8,7 +8,23 @@ def test_node_data():
 
 
 def test_node_add():
-    newNode = Node("Bob")
+    bob = Node("Bob")
     newList = LinkedList()
-    newList.insert(newNode)
+    newList.insert(bob)
     assert newList.firstNode.data == "Bob"
+
+
+def test_next_node_none():
+    bob = Node("Bob")
+    newList = LinkedList()
+    newList.insert(bob)
+    assert newList.firstNode.nextNode is None
+
+
+def test_add_second_node():
+    joe = Node("Joe")
+    bob = Node("Bob")
+    newList = LinkedList()
+    newList.insert(bob)
+    newList.insert(joe)
+    assert newList.firstNode.nextNode.data == "Bob"
