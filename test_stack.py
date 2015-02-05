78a23e7a1ab8644834a4981e2320885b5865e5fa
James Warren <jwarren116@gmail.com>
2015-02-05 14:32:21 -0800

    Add tests for creating stack frame and pushing/popping multiple values from stack

--- test_stack.py
+++ test_stack.py
@@ -9,11 +9,31 @@ def test_item_data():
     assert bacon.data == "Bacon"
 
 
+def test_create_stack_frame():
+    # Tests that an empty stack can be created
+    new_stack = StackFrame()
+    assert new_stack
+
+
 def test_stack_push():
     # Tests that "Bacon" is first item when pushed to stack
     bacon = StackItem("Bacon")
     new_stack = StackFrame()
     new_stack.push(bacon)
+    assert new_stack.first_item.data == "Bacon"
+
+
+def test_stack_push_multi():
+    # Tests that we can push multiple items to stack, then get expected results from pop
+    bacon = StackItem("Bacon")
+    steak = StackItem("Steak")
+    grilled_cheese = StackItem("Grilled Cheese")
+    new_stack = StackFrame()
+    new_stack.push(bacon)
+    new_stack.push(steak)
+    new_stack.push(grilled_cheese)
+    new_stack.pop()
+    new_stack.pop()
     assert new_stack.first_item.data == "Bacon"
 
 
