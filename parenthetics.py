d4a9c6b883ba05ee3e6fc02cbb9ec852dd676e9e
Jakealope <jake.anderson486@gmail.com>
2015-02-08 19:54:45 -0800

    adjusted naming system and class

--- parenthetics.py
+++ parenthetics.py
@@ -1,10 +1,12 @@
 
 
-def isequal(text):
-    count = 0
-    for par in text:
-        if par == '(':
-            count += 1
-        if par == ')':
-            count -= 1
-        return count
+class Parenthentic(object):
+
+    def isequal(self, text):
+        count = 0
+        for par in text:
+            if par == '(':
+                count += 1
+            if par == ')':
+                count -= 1
+            return count
