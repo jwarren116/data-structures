4b093e177110f8df100ca40337f5a41c4b6b9084
James Warren <jwarren116@gmail.com>
2015-02-08 21:11:54 -0800

    Clean up code; include docstring explaining return values

--- parenthetics.py
+++ parenthetics.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python
 
 
-"""Proper Parenthetics
-Return 1 if the string is "open" (there are open parens that are not closed)
-
-Return 0 if the string is "balanced" (there are an equal number of open and
-closed parentheses in the string)
-
-Return -1 if the string is "broken" (a closing parens has not been proceeded
-by one that opens)
-"""
-
-
 def parenthetics(inp):
+    """Checks for balanced parenthetic usage in a given input
+
+    Returns 1 if the string has open parenthetics that are not properly closed.
+    Returns 0 if the string is balanced.
+    Returns -1 if the string has closing parentheses before opening.
+
+    """
     count = 0
     for char in inp:
         if char == "(":
@@ -21,10 +17,7 @@ def parenthetics(inp):
             count -= 1
             if count < 0:
                 return -1
-
     if count >= 1:
         return 1
-    elif count <= -1:
-        return -1
     else:
         return 0
