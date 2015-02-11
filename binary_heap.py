df578dfa4ca9cbdd974f5e2b9629f92ababe144a
James Warren <jwarren116@gmail.com>
2015-02-11 13:21:03 -0800

    Basic module outline of binary heap and test modules

---
+++ binary_heap.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+
+class BinaryHeap(object):
+    def __init__(self):
+        heap_list = []
+
+    def push(self, val):
+        """puts a new value at the top of the heap, maintaining the heap property"""
+        pass
+
+    def pop(self):
+        """removes the 'top' value in the heap, maintaining the heap property"""
+        pass
