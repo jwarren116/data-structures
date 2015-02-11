1cd74ac6d5065bd8c9ab97b4d25348d5d148882f
Jakealope <jake.anderson486@gmail.com>
2015-02-11 15:04:54 -0800

    added push func, passes

--- binary_heap.py
+++ binary_heap.py
@@ -6,9 +6,28 @@ class BinaryHeap(object):
         self.heap_list = []
 
     def push(self, val):
-        """puts a new value at the top of the heap, maintaining the heap property"""
-        pass
+        """puts a new value at the top of the heap,
+        maintaining the heap property"""
+        self.heap_list.append(val)
+        child_position = len(self.heap_list) - 1
+        if len(self.heap_list) == 1:
+            return
+        while True:
+            parent_position = (child_position - 1) / 2
+            parent = self.heap_list[parent_position]
+            if val <= parent:
+                break
+            else:
+                self.heap_list[parent_position] = val
+                self.heap_list[child_position] = parent
+                child_position = parent_position
+                if child_position == 0:
+                    break
 
     def pop(self):
-        """removes the 'top' value in the heap, maintaining the heap property"""
+        """removes the 'top' value in the heap,
+        maintaining the heap property"""
+        pass
+
+    def swap(self, child, parent):
         pass
