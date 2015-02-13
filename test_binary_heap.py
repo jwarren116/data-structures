f91be08a6c40914cc6dc42d57def09e9952dbb19
Jakealope <jake.anderson486@gmail.com>
2015-02-12 17:15:40 -0800

    added tests to make sure it all works, passing

--- test_binary_heap.py
+++ test_binary_heap.py
@@ -1,18 +1,19 @@
-from binary_heap import BinaryHeap
+from bin_heap import BinaryHeap
 import pytest
 
 
 def test_0():
     bin_list = BinaryHeap()
-    assert bin_list.heap_list == []
+    assert bin_list.heaplist == [0]
 
 
 def test_1():
     bin_list = BinaryHeap()
     bin_list.push(10)
     bin_list.push(9)
-    bin_list.push(5)
-    assert len(bin_list.heap_list) == 3
+    bin_list.push(4)
+    bin_list.push(72)
+    assert len(bin_list.heaplist) == 5
 
 
 def test_2():
@@ -26,24 +27,29 @@ def test_2():
     assert bin_list.pop() == 9
 
 
-# def test_3():
+def test_3():
+    bin_list = BinaryHeap()
+    bin_list.push(10)
+    assert bin_list.pop() == 10
+
+
+def test_4():
+    bin_list = BinaryHeap()
+    bin_list.push(10)
+    bin_list.push(9)
+    bin_list.push(5)
+    bin_list.push(17)
+    bin_list.push(28)
+    bin_list.push(6)
+    assert bin_list.pop() == 5
+
+
+def test_5():
+    bin_list = BinaryHeap()
+    with pytest.raises(IndexError):
+        bin_list.pop()
+
+
+# def test_6():
 #     bin_list = BinaryHeap()
-#     bin_list.push(10)
-#     assert bin_list.pop() == 10
-
-
-# def test_4():
-#     bin_list = BinaryHeap()
-#     bin_list.push(10)
-#     bin_list.push(9)
-#     bin_list.push(5)
-#     bin_list.push(17)
-#     bin_list.push(28)
-#     bin_list.push(6)
-#     assert bin_list.pop() == 5
-
-
-# def test_5():
-#     bin_list = BinaryHeap()
-#     with pytest.raises(ValueError):
-#         bin_list.pop()
+#     assert [bin_list.pop() for i in range(3)] == [1, 2, 3, 4]
