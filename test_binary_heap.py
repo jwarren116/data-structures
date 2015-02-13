362553d95e2d38c284cb7033d9d396ac5e501b90
Jakealope <jake.anderson486@gmail.com>
2015-02-13 15:07:06 -0800

    Created tests to check the full fuctionality of the heap

--- test_binary_heap.py
+++ test_binary_heap.py
@@ -1,5 +1,17 @@
 from bin_heap import BinaryHeap
 import pytest
+import random
+
+
+def test_is_empty():
+    bin_list = BinaryHeap()
+    assert bin_list.heaplist == [0]
+
+
+def test_one_push_pop():
+    bin_list = BinaryHeap()
+    bin_list.push(10)
+    assert bin_list.pop() == 10
 
 
 def all_items(heap):
@@ -19,60 +31,22 @@ def test_reverse_range():
     assert all_items(heap) == range(1, 6)
 
 
-# def test_with_range():
-#     bh = BinaryHeap()
-#     bh.push(range_func)
-#     assert all_items(5) == range(0, 5)
+def test_load_up_the_list():
+    heap = BinaryHeap()
+    for i in range(10000, 0, -1):
+        heap.push(i)
+    assert all_items(heap) == range(1, 10001)
 
 
-def test_0():
-    bin_list = BinaryHeap()
-    assert bin_list.heaplist == [0]
+def test_random_input():
+    h = BinaryHeap()
+    for i in random.sample(range(100), 100):
+        h.push(i)
+    assert all_items(h) == range(0, 100)
 
 
-def test_1():
-    bin_list = BinaryHeap()
-    bin_list.push(10)
-    bin_list.push(9)
-    bin_list.push(4)
-    bin_list.push(72)
-    assert len(bin_list.heaplist) == 5
-
-
-def test_2():
-    bin_list = BinaryHeap()
-    bin_list.push(10)
-    bin_list.push(9)
-    bin_list.push(5)
-    bin_list.push(7)
-    bin_list.pop()
-    bin_list.pop()
-    assert bin_list.pop() == 9
-
-
-def test_3():
-    bin_list = BinaryHeap()
-    bin_list.push(10)
-    assert bin_list.pop() == 10
-
-
-def test_4():
-    bin_list = BinaryHeap()
-    bin_list.push(10)
-    bin_list.push(9)
-    bin_list.push(5)
-    bin_list.push(17)
-    bin_list.push(28)
-    bin_list.push(6)
-    assert bin_list.pop() == 5
-
-
-def test_5():
-    bin_list = BinaryHeap()
-    with pytest.raises(IndexError):
-        bin_list.pop()
-
-
-# def test_6():
-#     bin_list = BinaryHeap()
-#     assert [bin_list.pop() for i in range(3)] == [1, 2, 3, 4]
+def test_really_really_big_random_input():
+    h = BinaryHeap()
+    for i in random.sample(range(1000000), 1000000):
+        h.push(i)
+    assert all_items(h) == range(0, 1000000)
