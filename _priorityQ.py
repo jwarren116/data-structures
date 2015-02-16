54c46aaafe5b99b4da01911b4133dfaace7f6a43
James Warren <jwarren116@gmail.com>
2015-02-16 13:40:53 -0800

    Fix typo; bin_heap will now properly instate

--- _priorityQ.py
+++ _priorityQ.py
@@ -4,7 +4,7 @@ import bin_heap as bh
 class Priority(object):
     """docstring for priority"""
     def __init__(self):
-        self.bin_heap = bh
+        self.bin_heap = bh()
 
     def insert(self, item, rank):
         bh.push(item, rank)
