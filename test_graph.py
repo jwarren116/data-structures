62793f5881b383cfa8d77c52e9d6c76ea79d85f4
Jakealope <jake.anderson486@gmail.com>
2015-02-17 14:46:43 -0800

    wrote first test, failing

--- test_graph.py
+++ test_graph.py
@@ -1,1 +1,10 @@
+from graph import SimpleGraph
 import pytest
+
+
+def test_add_nodes():
+    G = SimpleGraph()
+    G.add_node([A, B])
+    G.add_node([A, C])
+    G.add_node([B, D])
+    assert SimpleGraph() == ([A, B], [A, C], [B, D])
