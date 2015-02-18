093571645a33ccda326c4a6d507829e113aa9728
James Warren <jwarren116@gmail.com>
2015-02-17 21:45:53 -0800

    Add tests for graph

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 This repository will hold example code for a number of classic data structures.
 
+We have included a basic graph data structure. Our graph is capable of adding vertices/nodes, adding edges between nodes, deleting nodes and edges, reporting all connected nodes, and reporting nodes connected to it. As an inspiration, we used [this educational site](http://www.python-course.eu/graphs_python.php).
+
 
 This repository is a collaboration between Jake Anderson and James Warren while attending the Code Fellows Python Development Accelerator in Winter 2015.
--- test_graph.py
+++ test_graph.py
@@ -2,12 +2,64 @@ from graph import SimpleGraph
 import pytest
 
 
+test_dict = {"a": ["d"],
+             "b": ["c"],
+             "c": ["b", "c", "d", "e"],
+             "d": ["a", "c"],
+             "e": ["c"],
+             "f": []}
+
+
 def test_add_nodes():
+    g = SimpleGraph(test_dict)
+    assert 'a' in g.node()
+    assert 'b' in g.node()
+    assert 'c' in g.node()
+    assert 'd' in g.node()
+    assert 'e' in g.node()
+    assert 'f' in g.node()
+
+
+def test_del_nodes():
+    g = SimpleGraph(test_dict)
+    g.del_node('a')
+    assert 'a' not in g.node()
+    assert g.has_node('a') is False
+    assert g.node() == ['c', 'b', 'e', 'd', 'f']
+    g.add_node('a')
+
+
+def test_del_edge():
+    g = SimpleGraph(test_dict)
+    g.del_edge('c', 'd')
+    assert g.neighbors('c') == ['b', 'c', 'e']
+
+
+def test_add_edge():
+    g = SimpleGraph(test_dict)
+    g.add_edge('q', 'a')
+    assert g.has_node('q')
+    assert g.neighbors('q') == ['a']
+
+
+def test_has_node():
+    g = SimpleGraph(test_dict)
+    assert g.has_node('b')
+
+
+def test_adjacent():
+    g = SimpleGraph(test_dict)
+    assert g.adjacent('a', 'd')
+    assert not g.adjacent('a', 'f')
+
+
+def test_edges():
     g = SimpleGraph()
-    {"a": ["d"],
-        "b": ["c"],
-        "c": ["b", "c", "d", "e"],
-        "d": ["a", "c"],
-        "e": ["c"],
-        "f": []}
-    assert g.node() == ['a', 'c', 'b', 'e', 'd', 'f']
+    g.add_node('a')
+    g.add_node('b')
+    g.add_node('c')
+    g.add_edge('a', 'c')
+    g.add_edge('a', 'b')
+    assert g.has_node('a')
+    assert  'c' in g.node()
+    assert g.edges() == '''b->[]  c->[]  a->['c', 'b']  '''
