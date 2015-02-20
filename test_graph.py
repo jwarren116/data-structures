bf719c1913eff266d91033d0fc8514f19778c28d
James Warren <jwarren116@gmail.com>
2015-02-20 15:36:35 -0800

    Fix tests for graph to not accept pre populated dictionary

--- test_graph.py
+++ test_graph.py
@@ -2,71 +2,105 @@ from graph import SimpleGraph
 import pytest
 
 
-test_dict = {"a": ["d"],
-             "b": ["c"],
-             "c": ["b", "c", "d", "e"],
-             "d": ["a", "c"],
-             "e": ["c"],
-             "f": []}
-
-
 def test_add_nodes():
-    g = SimpleGraph(test_dict)
-    assert 'a' in g.node()
-    assert 'b' in g.node()
-    assert 'c' in g.node()
-    assert 'd' in g.node()
-    assert 'e' in g.node()
-    assert 'f' in g.node()
+    g = SimpleGraph()
+    g.add_node('a')
+    g.add_node('b')
+    g.add_node('c')
+    g.add_edge('a', 'c')
+    g.add_edge('a', 'b')
+    assert 'a' in g.nodes()
+    assert 'b' in g.nodes()
+    assert 'c' in g.nodes()
 
 
 def test_del_nodes():
-    g = SimpleGraph(test_dict)
-    g.del_node('a')
-    assert 'a' not in g.node()
-    assert g.has_node('a') is False
-    assert g.node() == ['c', 'b', 'e', 'd', 'f']
+    g = SimpleGraph()
     g.add_node('a')
+    g.add_node('b')
+    g.add_node('c')
+    g.add_edge('a', 'c')
+    g.add_edge('a', 'b')
+    g.del_node('a')
+    assert 'a' not in g.nodes()
+    assert g.has_node('a') is False
+    assert g.nodes() == ['c', 'b']
 
 
 def test_del_error():
-    g = SimpleGraph(test_dict)
+    g = SimpleGraph()
+    g.add_node('a')
+    g.add_node('b')
+    g.add_node('c')
+    g.add_edge('a', 'c')
+    g.add_edge('a', 'b')
     with pytest.raises(KeyError):
         g.del_node('p')
 
 
 def test_del_edge():
-    g = SimpleGraph(test_dict)
-    g.del_edge('c', 'd')
-    assert g.neighbors('c') == ['b', 'c', 'e']
+    g = SimpleGraph()
+    g.add_node('a')
+    g.add_node('b')
+    g.add_node('c')
+    g.add_edge('a', 'c')
+    g.add_edge('a', 'b')
+    g.del_edge('a', 'c')
+    assert g.neighbors('a') == ['b']
 
 
 def test_edge_error():
-    g = SimpleGraph(test_dict)
+    g = SimpleGraph()
+    g.add_node('a')
+    g.add_node('b')
+    g.add_node('c')
+    g.add_edge('a', 'c')
+    g.add_edge('a', 'b')
     with pytest.raises(ValueError):
         g.del_edge('a', 'z')
 
 
 def test_add_edge():
-    g = SimpleGraph(test_dict)
+    g = SimpleGraph()
+    g.add_node('a')
+    g.add_node('b')
+    g.add_node('c')
+    g.add_edge('a', 'c')
+    g.add_edge('a', 'b')
     g.add_edge('q', 'a')
     assert g.has_node('q')
     assert g.neighbors('q') == ['a']
 
 
 def test_has_node():
-    g = SimpleGraph(test_dict)
+    g = SimpleGraph()
+    g.add_node('a')
+    g.add_node('b')
+    g.add_node('c')
+    g.add_edge('a', 'c')
+    g.add_edge('a', 'b')
     assert g.has_node('b')
 
 
 def test_adjacent():
-    g = SimpleGraph(test_dict)
+    g = SimpleGraph()
+    g.add_node('a')
+    g.add_node('b')
+    g.add_node('c')
+    g.add_edge('a', 'd')
+    g.add_edge('a', 'f')
+    assert 'f' in g.nodes()
     assert g.adjacent('a', 'd')
-    assert not g.adjacent('a', 'f')
+    assert not g.adjacent('a', 'b')
 
 
 def test_adjacent_error():
-    g = SimpleGraph(test_dict)
+    g = SimpleGraph()
+    g.add_node('a')
+    g.add_node('b')
+    g.add_node('c')
+    g.add_edge('a', 'c')
+    g.add_edge('a', 'b')
     with pytest.raises(KeyError):
         g.adjacent('l', 'm')
 
@@ -78,6 +112,5 @@ def test_edges():
     g.add_node('c')
     g.add_edge('a', 'c')
     g.add_edge('a', 'b')
-    assert g.has_node('a')
-    assert 'c' in g.node()
-    assert g.edges() == '''b->[]  c->[]  a->['c', 'b']  '''
+    assert ('a', 'c') in g.edges()
+    assert ('a', 'b') in g.edges()
