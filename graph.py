f461cb45f84779a125d1ae4e99bef47251875a17
Jakealope <jake.anderson486@gmail.com>
2015-03-03 21:33:45 -0800

    first attempt at short path.. epically failing

--- graph.py
+++ graph.py
@@ -1,3 +1,6 @@
+import heapq
+import sys
+
 
 class SimpleGraph(object):
     '''This is a simple graph program that will allow us
@@ -87,5 +90,40 @@ class SimpleGraph(object):
         self._depth_first_visitor(start, set(), return_value)
         return return_value
 
-    def dijkstra(self, start, destination):
-        pass
+
+def dijkstra(self, start, finish):
+    distances = {}
+    previous = {}
+    nodes = []
+
+    for edge in self.vertices:
+        if edge == start:
+            distances[edge] = 0
+            heapq.heappush(nodes, [0, edge])
+        else:
+            distances[edge] = sys.maxint
+            heapq.heappush(nodes, [sys.maxint, edge])
+        previous[edge] = None
+
+    while nodes:
+        smallest = heapq.heappop(nodes)[1]
+        if smallest == finish:
+            path = []
+            while previous[smallest]:
+                path.append(smallest)
+                smallest = previous[smallest]
+            return path
+        if distances[smallest] == sys.maxint:
+            break
+
+        for neighbor in self.vertices[smallest]:
+            alt = distances[smallest] + self.vertices[smallest][neighbor]
+            if alt < distances[neighbor]:
+                distances[neighbor] = alt
+                previous[neighbor] = smallest
+                for n in nodes:
+                    if n[1] == neighbor:
+                        n[0] = alt
+                        break
+                heapq.heapify(nodes)
+    print distances
