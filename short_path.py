dc0444a272dca9ed5f005ee8e33e693bfc12da41
Jakealope <jake.anderson486@gmail.com>
2015-03-06 09:44:07 -0800

    implemented algorithm, not passing

--- short_path.py
+++ short_path.py
@@ -1,54 +1,40 @@
-import heapq
+from bin_heap import BinaryHeap
+from graph import SimpleGraph
 import sys
 # from graph import add_edges
 
 
-def dijkstra(graph, src, dest, visited=[], distances={}, predecessors={}):
+def dijkstra(graph, src, dest):
+    unvisited = {node: float('inf') for node in nodes}
+    visited = {}
+    current = src
+    current_distance = 0
+    unvisited[current] = current_distance
 
-    if src not in graph:
-        raise TypeError('the root of the shortest path tree cannot be found in the graph')
-        if dest not in graph:
-            raise TypeError('the target of the shortest path cannot be found in the graph')
-    # ending condition
-    if src == dest:
-        # We build the shortest path and display it
-        path = []
-        pred = dest
-        while pred is not None:
-            path.append(pred)
-            pred = predecessors.get(pred, None)
-        if pred is None:
-            print path, distances[dest]
-    else:
-        if not visited:
-            distances[src] = 0
-        # visit the neighbors
-        for neighbor in graph[src]:
-            if neighbor not in visited:
-                new_distance = distances[src] + graph[src][neighbor]
-                if new_distance < distances.get(neighbor, float('inf')):
-                    distances[neighbor] = new_distance
-                    predecessors[neighbor] = src
-        # mark as visited
-        visited.append(src)
-        # now that all neighbors have been visited: recurse
-        # select the non visited node with lowest distance 'x'
-        # run Dijskstra with src='x'
-        unvisited = {}
-        for k in graph:
-            if k not in visited:
-                unvisited[k] = distances.get(k, float('inf'))
-                x = min(unvisited, key=unvisited.get)
-                dijkstra(graph, x, dest, visited, distances, predecessors)
+    while True:
+        for neighbor, distance in distances[current].items():
+            if neighbor not in unvisited:
+                continue
+            new_distance = current_distance + distance
+            if unvisited[neighbor] is float('inf') or unvisited[neighbor] > new_distance:
+                unvisited[neighbor] = new_distance
+        visited[current] = current_distance
+        del unvisited[current]
+        if not unvisited:
+            break
+        candidates = [node for node in unvisited.items() if node[1]]
+        current, current_distance = sorted(candidates, key=lambda x: x[1])[0]
+    print(current_distance)
 
 
-if __name__ == "__main__":
-    # import sys;sys.argv = ['', 'Test.testName']
-    # unittest.main()
-    graph = {'s': {'a': 2, 'b': 1},
-             'a': {'s': 3, 'b': 4, 'c': 8},
-             'b': {'s': 4, 'a': 2, 'd': 2},
-             'c': {'a': 2, 'd': 7, 't': 4},
-             'd': {'b': 1, 'c': 11, 't': 5},
-             't': {'c': 3, 'd': 5}}
-    dijkstra(graph, 's', 't')
+if __name__ == '__main__':
+    nodes = ('A', 'B', 'C', 'D', 'E', 'F', 'G')
+    distances = {
+        'B': {'A': 5, 'D': 1, 'G': 2},
+        'A': {'B': 5, 'D': 3, 'E': 12, 'F': 5},
+        'D': {'B': 1, 'G': 1, 'E': 1, 'A': 3},
+        'G': {'B': 2, 'D': 1, 'C': 2},
+        'C': {'G': 2, 'E': 1, 'F': 16},
+        'E': {'A': 12, 'D': 1, 'C': 1, 'F': 2},
+        'F': {'A': 5, 'E': 2, 'C': 16}
+    }
